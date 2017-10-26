f856f7713b75216ab6810170ad13760ff919bd32
axecopfire <braun.sn11@gmail.com>
2017-10-26 09:18:34 -0400

    navbar turned into a string and put in index.js

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+    document.getElementById("navHome").innerHTML = `<nav class="navbar-default">
+    <div class="navbar-header nvhead">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" target="#mynavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>                        
+        <a class="nvbar-brand">
+            <img src="images/Earth_Globe.png">
+        </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-btn btn nvbrlinks">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about-us.html">About Us</a></li>
+            <li><a href="RCLA.html">Language Center</a></li>
+            <li><a href="Translation.html">Translation and Interpretation</a></li>
+            <li><a href="Employment.html">Employment Opportunities</a></li>
+            <li><a href="Contact.html">Contact Us</a></li>
+        </ul>
+    </div>
+</nav>`;
+

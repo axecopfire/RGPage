852d26e5c3e74707778f414fefedff33687bddd7
axecopfire <axecopfire@users.noreply.github.com>
2017-11-03 23:32:16 -0400

    First work

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,22 @@
-document.getElementById("navHome").innerHTML = `    <nav class="navbar-default">
-<div class="navbar-header nvhead">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" target="#mynavbar" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-    </button>                        
-    <a class="nvbar-brand">
-        <img src="images/Earth_Globe.png">
-    </a>
-</div>
-<div class="collapse navbar-collapse" id="myNavbar">
-    <ul class="nav navbar-nav navbar-btn btn nvbrlinks">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about-us.html">About Us</a></li>
-        <li><a href="RCLA.html">Language Center</a></li>
-        <li><a href="Translation.html">Translation and Interpretation</a></li>
-        <li><a href="Employment.html">Employment Opportunities</a></li>
-        <li><a href="Contact.html">Contact Us</a></li>
-        <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                Endeavors <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-                <li><a href="#">Language Center Website</a></li>
-                <li><a href="#">Technical Training Website</a></li>
-            </ul>
-        </li>
-    </ul>
-</div>
-</nav>`;
+document.addEventListener("DOMContentLoaded", function() {
+    console.log(decodeURI(window.location.pathname));
+    var currentPath = window.location.pathname.slice(window.location.pathname.lastIndexOf("/"), window.location.pathname.length);
+    console.log(currentPath);
+
+    var content = document.getElementById("webpage");
+
+
+
+    /****************************** */
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'http://Users/Houndor/Desktop/Github/RGPage/index.html', true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            content.innerHTML = request.responseText;
+        }
+    };
+
+    request.send();
+});
